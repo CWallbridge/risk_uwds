7a4649c5c8c97f41f614e9b32e634648284aab93
cwallbridge <chris.wallbridge@googlemail.com>
2021-06-21 11:48:45 +0100

    Added main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import underworlds
+import underworlds.server
+from underworlds.tools.loader import ModelLoader
+from underworlds.tools.spatial_relations import *
+from underworlds.tools.edit import *
+from underworlds.helpers.geometry import get_world_transform
+from underworlds.helpers.transformations import *
+
+import rospy
+import tf
+import numpy
+import math
+import random
+import time
+import sys
+import os
+import csv
+
+if __name__ == "__main__":
+	pass
